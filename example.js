e5445d66af61fd04b3a27fccadc503f2fd8544b7
leanne <leanne.lacey@justsomecoding.com>
2018-12-03 16:53:04 +0100

    done using a for loop

--- example.js
+++ example.js
@@ -7,15 +7,8 @@ var animals = [
   { name: "Nono", species: "horse" }
 ];
 
-//better way to do this is with functional programming, the concept behind JS
-var isDog = function(animal) {
-  return animal.species === "dog";
-};
-console.log(dogs);
-var dogs = animals.filter(isDog);
-
-// var dogs = [];
-// for (var i = 0; i < animals.length; i++) {
-//   if (animals[i].species === "dog") dogs.push(animals[i]);
-// }
-// console.log(dogs);
+var names = [];
+for (var i = 0; i < animals.length; i++) {
+  names.push(animals[i].name);
+}
+console.log(names);
