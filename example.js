b39c1a59d6d617488c8526b3ea4c24dd4d75f16b
Leannelb <leannelaceybyrne@outlook.com>
2018-12-03 20:14:19 +0100

    using reduce instead to add values

--- example.js
+++ example.js
@@ -6,14 +6,19 @@ var orders = [
   { amount: 100 }
 ];
 
+var totalAmount = orders.reduce(function(sum, order) {
+  return sum + order.amount;
+}, 0);
+
 //map and filter are functions that can be used on an array object
 //map filter reject and find are all list transformations - turn your list into something else
 //reduce is not.. reduce is the multi-tool of list transformations
 //reduce can be used to implement fuctions like map, filter, find etc..
 // can be used if you cant find a suitable inbuilt function
 
-var totalAmount = 0;
+//using a for loop
+/*var totalAmount = 0;
 for (var i = 0; i < orders.length; i++) {
   totalAmount += orders[i].amount;
-}
+}*/
 console.log(totalAmount);
