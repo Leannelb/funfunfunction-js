8e6ba73f7f2a55c5bba225bc0be4b857ca016c32
leanne <leanne.lacey@justsomecoding.com>
2018-12-03 16:30:08 +0100

    using a function instead - works better

--- example.js
+++ example.js
@@ -7,8 +7,15 @@ var animals = [
   { name: "Nono", species: "horse" }
 ];
 
-var dogs = [];
-for (var i = 0; i < animals.length; i++) {
-  if (animals[i].species === "dog") dogs.push(animals[i]);
-}
+//better way to do this is with functional programming, the concept behind JS
+
+var dogs = animals.filter(function(animal) {
+  return animal.species === "dog";
+});
 console.log(dogs);
+
+// var dogs = [];
+// for (var i = 0; i < animals.length; i++) {
+//   if (animals[i].species === "dog") dogs.push(animals[i]);
+// }
+// console.log(dogs);
