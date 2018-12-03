4ec247c31830d7d14dbce825245f222b513ceaac
leanne <leanne.lacey@justsomecoding.com>
2018-12-03 16:05:39 +0100

    inital commit

---
+++ example.js
@@ -0,0 +1,8 @@
+var animals = [
+  { name: "Tom", species: "rabbit" },
+  { name: "Sam", species: "dog" },
+  { name: "Mary", species: "rabbit" },
+  { name: "Sprite", species: "dog" },
+  { name: "Koodo", species: "horse" },
+  { name: "Nono", species: "horse" }
+];
