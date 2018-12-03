3f2967e2587e9b281d09b5f0c472ebd327ca372c
Leannelb <leannelaceybyrne@outlook.com>
2018-12-03 21:36:59 +0100

    functions taking in multiple arguments

--- example.js
+++ example.js
@@ -1,20 +1,8 @@
-// // ES6 way below -newer but may not always work yet
-// import fs from "fs";
-//common js syntax below
-const fs = require("fs");
+//currying in js
+let dragon = (name, size, element) =>
+  name + " is a " + size + " dragon that breathes " + element + "!";
 
-var output = fs
-  .readFileSync("data.txt", "utf8")
-  .trim() //gets rid of the extra extra line at the end
-  .split("\n") //splits it up with a new line
-  .map(line => line.split("\t")); //this has given us an array of arrays!!!
+console.log(dragon("fluffykins", "tiny", "lighning"));
 
-console.log("output", output);
-
-// output
-// mark jonson waffle iron    80  2
-// mark jonson blender    200  1
-// mark jonson knife    10  4
-// Nikita Smith    waffle iron 80  1
-// Nikita Smith    knife 10  2
-// Nikita Smith    pot 20  3
+// outputs
+//fluffykins is a tiny dragon that breathes lighning!
